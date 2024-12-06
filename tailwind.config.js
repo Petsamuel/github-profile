1b30b37e2702d2f3fd785fab2e5fb35b5a82f979
petsamuel <petsamuel4@gmail.com>
2024-12-06 03:00:12 +0100

    ✨ add hero background image to CSS and update Tailwind configuration for background image paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-// import HeroImage from "";
+
 export default {
   content: ["./src/**/*.{html,jsx,tsx,ts}"],
   theme: {
     extend: {
       backgroundImage: {
-        heroBg: "url('./src/assets/hero-image-github-profile.png')",
-        searchIcon: "url('./src/assets/Search.svg')",
+        heroBg: "url('/src/assets/hero-image-github-profile.png')",
+        searchIcon: "url('/src/assets/Search.svg')",
       },
     },
   },
